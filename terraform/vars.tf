4c9b0aa369ca159fe0b8431381e4e32cd0f03f35
Mpjames217 <171149609+Mpjames217@users.noreply.github.com>
2024-11-13 15:29:58 +0000

    different format attempted for env variables"

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,24 +1,24 @@
-variable "TF_VAR_PG_USER" {
-    type = string
+# variable "TF_VAR_PG_USER" {
+#     type = string
   
-}
+# }
 
-variable "TF_VAR_PG_PASSWORD" {
-    type = string
+# variable "TF_VAR_PG_PASSWORD" {
+#     type = string
   
-}
+# }
 
-variable "TF_VAR_PG_DATABASE" {
-    type = string
+# variable "TF_VAR_PG_DATABASE" {
+#     type = string
   
-}
+# }
 
-variable "TF_VAR_PG_PORT" {
-    type = string
+# variable "TF_VAR_PG_PORT" {
+#     type = string
   
-}
+# }
 
-variable "TF_VAR_PG_HOST" {
-    type = string
+# variable "TF_VAR_PG_HOST" {
+#     type = string
   
-}
+# }
