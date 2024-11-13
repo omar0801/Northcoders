5ecf394b975a063fe182f35a8bed304afdacb4d8
Mpjames217 <171149609+Mpjames217@users.noreply.github.com>
2024-11-13 14:42:37 +0000

    lambda iam resource corrected to have access to items inside bucket

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role" "lambda_role" {
 data "aws_iam_policy_document" "s3_document" {
   statement {
     actions = ["s3:GetObject", "s3:PutObject"]
-    resources = ["${aws_s3_bucket.ingestion-bucket.arn}"]
+    resources = ["${aws_s3_bucket.ingestion-bucket.arn}/*"]
     }
 }
 
