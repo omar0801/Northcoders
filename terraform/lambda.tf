b3fdfaaf4e02ea19c2a646a1fb21943cf5a803fb
Mpjames217 <171149609+Mpjames217@users.noreply.github.com>
2024-11-13 12:24:05 +0000

    layer added

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,6 +5,21 @@ data "archive_file" "lambda" {
   output_path = "${path.module}/../src/extract_data.zip"
 }
 
+data "archive_file" "layer" {
+  type = "zip"
+  output_file_mode = "0666"
+  source_dir = "${path.module}/../layer"
+  output_path = "${path.module}/../layer.zip"
+}
+
+resource "aws_lambda_layer_version" "project_layer" {
+  layer_name = "project_layer"
+  compatible_runtimes = "python3.12"
+  s3_bucket = aws_s3_bucket.lambda_code_bucket.id
+  s3_key = aws_s3_object.layer.key
+  
+}
+
 resource "aws_lambda_function" "dummy_lambda" {
   function_name = "extract_data"
   handler = "extract_data.main"
@@ -13,4 +28,5 @@ resource "aws_lambda_function" "dummy_lambda" {
   s3_bucket = aws_s3_bucket.lambda_code_bucket.id
   s3_key = aws_s3_object.lambda_code.key
   role = aws_iam_role.lambda_role.arn
+  layers = [aws_lambda_layer_version.project_layer.arn]
 }
