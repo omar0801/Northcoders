af6868015a0ea6d129d7365b882019832d2b13fb
Nana <nanagalaxy385@gmail.com>
2024-11-15 15:39:11 +0000

    main function -tested various behaviours

--- src/compare_changes.py
+++ src/compare_changes.py
@@ -1,3 +1,7 @@
+import boto3
+from datetime import datetime
+import json
+
 def check_additions(db_data, s3_data):
 
     change_log = {}
@@ -20,7 +24,8 @@ def check_additions(db_data, s3_data):
         change_log['message'] = "No addition found"
     
     temp_rec.reverse()
-    change_log['records'] = temp_rec 
+    if temp_rec:
+        change_log['records'] = temp_rec 
     
     return change_log
     
@@ -55,17 +60,76 @@ def check_changes(db_data, s3_data):
 
     changed_recs = []
     for id, data in db_dict.items():
-        if s3_dict[id] != data:
-            changed_rec = {'id': id}
-            for key in keys:
-                if s3_dict[id][key] != data[key]:
-                    changed_rec[key] = data[key]
-            changed_recs.append(changed_rec)
+        # if id not in list(s3_dict.values()):
+        try:
+            if s3_dict[id] != data:
+                changed_rec = {'id': id}
+                for key in keys:
+                    if s3_dict[id][key] != data[key]:
+                        changed_rec[key] = data[key]
+                changed_recs.append(changed_rec)
+        except KeyError:
+            continue
+
 
     if changed_recs:
         change_log['message'] = 'Changes detected'
         change_log['records'] = changed_recs  
     else:
         change_log['message'] = 'No changes detected'
+
         
     return change_log
+
+
+def main_check_for_changes(event, context, client):
+    db = event['db']
+    s3 = event['s3']
+
+    keys = list(s3.keys())
+    str_timestamp = datetime.now().isoformat()
+    change_detected = {}
+
+    for table in keys:
+        changes_rec = {}
+        changes_rec[table] = {}
+
+        addition_result = check_additions(db[table], s3[table])
+        if 'records' in list(addition_result.keys()):
+            changes_rec[table]['additions'] = addition_result['records']
+        else:
+            changes_rec[table]['additions'] = None
+        
+        deletions_result = check_deletions(db[table], s3[table])
+        if 'records' in list(deletions_result.keys()):
+            # change_detected['changes detected'] = True
+            changes_rec[table]['deletions'] = deletions_result['records']
+        else:
+            changes_rec[table]['deletions'] = None
+
+        changes_result = check_changes(db[table], s3[table])
+        if 'records' in list(changes_result.keys()):
+            # change_detected['changes detected'] = True
+            changes_rec[table]['changes'] = changes_result['records']
+        else:
+            changes_rec[table]['changes'] = None
+        if any(changes_rec[table].values()):
+            change_detected[table] = db[table]
+        
+
+        
+        data_JSON = json.dumps(changes_rec)
+        client.put_object(
+            Bucket= 'ingestion-bucket-neural-normalisers-new',
+            Body=data_JSON,
+            Key= f"changes_log/{str_timestamp}/{table}.json"
+            )
+        
+    # print(change_detected)
+    return change_detected
+
+
+        
+
+
+
