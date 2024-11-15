97d25f7df71b13e3277810bbf4224d973fc0b659
omar <omar_barouni@hotmail.com>
2024-11-15 14:01:01 +0000

    made chnages to terraform

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,14 @@
-data "archive_file" "lambda" {
+data "archive_file" "lambda_3" {
   type = "zip"
   output_file_mode = "0666"
   source_file = "${path.module}/../src/extract_data.py"
+  output_path = "${path.module}/../src/write_to_s3.zip"
+}
+
+data "archive_file" "lambda_1" {
+  type = "zip"
+  output_file_mode = "0666"
+  source_file = "${path.module}/../src/extract_json_ingestion_zone.py"
   output_path = "${path.module}/../src/extract_data.zip"
 }
 
@@ -20,13 +27,33 @@ resource "aws_lambda_layer_version" "project_layer" {
   
 }
 
-resource "aws_lambda_function" "extract_data" {
-  function_name = "extract_data"
+resource "aws_lambda_function" "write_to_s3" {
+  function_name = "write_to_s3"
   handler = "extract_data.main"
   runtime = "python3.12"
   timeout = 60
   s3_bucket = aws_s3_bucket.lambda_code_bucket.id
-  s3_key = aws_s3_object.lambda_code.key
+  s3_key = aws_s3_object.write_to_s3_lambda.key
+  role = aws_iam_role.lambda_role.arn
+  layers = [aws_lambda_layer_version.project_layer.arn]
+  environment {
+    variables = {
+      PG_HOST=var.PG_HOST
+      PG_PORT=var.PG_PORT
+      PG_DATABASE=var.PG_DATABASE
+      PG_USER=var.PG_USER
+      PG_PASSWORD=var.PG_PASSWORD
+    }
+  }
+}
+
+resource "aws_lambda_function" "extract_data" {
+  function_name = "extract_data"
+  handler = "extract_json_ingestion_zone.lambda_handler"
+  runtime = "python3.12"
+  timeout = 60
+  s3_bucket = aws_s3_bucket.lambda_code_bucket.id
+  s3_key = aws_s3_object.extract_data_lambda.key
   role = aws_iam_role.lambda_role.arn
   layers = [aws_lambda_layer_version.project_layer.arn]
   environment {
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,7 +6,14 @@ resource "aws_s3_bucket" "lambda_code_bucket" {
   bucket_prefix = "lambda-code-bucket"
 }
 
-resource "aws_s3_object" "lambda_code" {
+resource "aws_s3_object" "write_to_s3_lambda" {
+  bucket = aws_s3_bucket.lambda_code_bucket.id
+  key = "lambda_code"
+  source = "${path.module}/../src/write_to_s3.zip"
+}
+
+
+resource "aws_s3_object" "extract_data_lambda" {
   bucket = aws_s3_bucket.lambda_code_bucket.id
   key = "lambda_code"
   source = "${path.module}/../src/extract_data.zip"
