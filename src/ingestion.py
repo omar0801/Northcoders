fc86bbe9623f272bf619109922edd46644eec523
Mpjames217 <171149609+Mpjames217@users.noreply.github.com>
2024-11-18 11:56:57 +0000

    ingestion.py refactor and tests complete

---
+++ src/ingestion.py
@@ -0,0 +1,228 @@
+import boto3, json, logging, pg8000.native, os, pprint
+from datetime import datetime
+from decimal import Decimal
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+tables = [
+    "counterparty",
+    "currency",
+    "department",
+    "design",
+    "staff",
+    "sales_order",
+    "address",
+    "payment",
+    "purchase_order",
+    "payment_type",
+    "transaction"
+]
+
+def connect_to_db():
+    return pg8000.native.Connection(
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD"),
+        database=os.getenv("PG_DATABASE"),
+        host=os.getenv("PG_HOST"),
+        port=int(os.getenv("PG_PORT"))
+    )
+
+def close_db_connection(conn):
+    conn.close()
+
+def fetch_from_s3(bucket, key,s3 ):
+    logger.info('Lambda handler invoked with event: %s', json.dumps([bucket, key]))
+    try:
+        logger.info("Fetching object from S3: bucket=%s, key=%s", bucket, key)
+        response = s3.get_object(Bucket=bucket, Key=key)
+        content = json.loads(response['Body'].read().decode('utf-8'))
+        logger.info("Successfully retrieved and parsed object from S3")
+        return content
+    
+    except s3.exceptions.NoSuchKey:
+        logger.error("Object not found in S3: bucket=%s, key=%s", bucket, key)
+    except json.JSONDecodeError as e:
+        logger.error("Failed to decode JSON: %s", e)
+
+def fetch_data_from_table(conn, table_name):
+    query = f"SELECT * FROM {table_name};"
+    
+    result = conn.run(query)
+    columns = [col["name"] for col in conn.columns]
+    
+    data = []
+    for row in result:
+        row_dict = {}
+        for i, value in enumerate(row):
+            if isinstance(value, datetime):
+                row_dict[columns[i]] = value.isoformat()
+            elif isinstance(value, Decimal):
+                row_dict[columns[i]] = float(value)
+            else:
+                row_dict[columns[i]] = value
+        data.append(row_dict)
+    
+    return data
+
+def get_latest_s3_keys(bucket,s3_client):
+    all_objects = s3_client.list_objects_v2(Bucket=bucket)
+    all_key_timestamps = [item['Key'][-31:-5] for item in all_objects['Contents'] if 'changes_log' not in item['Key']]
+    latest_timestamp = sorted(all_key_timestamps, reverse=True)[0]
+    return latest_timestamp
+
+def save_to_s3(data, bucket_name, filename, client):
+    data_JSON = json.dumps(data)
+    client.put_object(
+        Bucket=bucket_name,
+        Body=data_JSON,
+        Key=filename
+    )
+
+def check_additions(db_data, s3_data):
+
+    change_log = {}
+    if not db_data:
+        change_log['message'] = 'No records found in database'
+        return change_log
+    
+    last_record = s3_data[-1]
+    last_id = list(last_record.values())[0]    
+    temp_rec = []
+    for rec in reversed(db_data):
+        id_value = list(rec.values())[0]
+        if id_value > last_id:
+            temp_rec.append(rec)
+            change_log['message'] = "Addition detected" 
+        else:
+            break
+    
+    if not temp_rec:
+        change_log['message'] = "No addition found"
+    
+    temp_rec.reverse()
+    if temp_rec:
+        change_log['records'] = temp_rec 
+    
+    return change_log
+    
+def check_deletions(db_data, s3_data):
+    
+    delete_log = {}
+
+    s3_id_list = [list(rec.values())[0] for rec in s3_data]
+    db_id_list = [list(rec.values())[0] for rec in db_data]
+
+    deleted_list = []
+    for item in s3_id_list:
+        if item not in db_id_list:
+            deleted_list.append(item)
+            delete_log['message'] = 'Deletion detected'
+    
+    if deleted_list:
+        delete_log['records'] = deleted_list
+    else:
+        delete_log['message'] = 'No deletions detected'
+
+    return delete_log
+    
+def check_changes(db_data, s3_data):
+    change_log = {}
+
+    s3_dict = {list(rec.values())[0]: rec for rec in s3_data}
+    db_dict = {list(rec.values())[0]: rec for rec in db_data}
+    keys = list(s3_data[0].keys())
+
+    changed_recs = []
+    for id, data in db_dict.items():
+        # if id not in list(s3_dict.values()):
+        try:
+            if s3_dict[id] != data:
+                changed_rec = {'id': id}
+                for key in keys:
+                    if s3_dict[id][key] != data[key]:
+                        changed_rec[key] = data[key]
+                changed_recs.append(changed_rec)
+        except KeyError:
+            continue
+
+
+    if changed_recs:
+        change_log['message'] = 'Changes detected'
+        change_log['records'] = changed_recs  
+    else:
+        change_log['message'] = 'No changes detected'
+
+        
+    return change_log
+
+def main_check_for_changes(event, client):
+    db = event['db']
+    s3 = event['s3']
+
+    keys = list(s3.keys())
+    str_timestamp = datetime.now().isoformat()
+    change_detected = []
+
+    for table in keys:
+        changes_rec = {}
+        changes_rec[table] = {}
+
+        addition_result = check_additions(db[table], s3[table])
+        if 'records' in list(addition_result.keys()):
+            changes_rec[table]['additions'] = addition_result['records']
+        else:
+            changes_rec[table]['additions'] = None
+        
+        deletions_result = check_deletions(db[table], s3[table])
+        if 'records' in list(deletions_result.keys()):
+            changes_rec[table]['deletions'] = deletions_result['records']
+        else:
+            changes_rec[table]['deletions'] = None
+
+        changes_result = check_changes(db[table], s3[table])
+        if 'records' in list(changes_result.keys()):
+            changes_rec[table]['changes'] = changes_result['records']
+        else:
+            changes_rec[table]['changes'] = None
+        if any(changes_rec[table].values()):
+            change_detected.append(table)
+        
+
+        
+        data_JSON = json.dumps(changes_rec)
+        client.put_object(
+            Bucket= 'ingestion-bucket-neural-normalisers-new',
+            Body=data_JSON,
+            Key= f"changes_log/{table}/{str_timestamp}.json"
+            )
+    
+    return change_detected
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    conn = connect_to_db()
+    data = {'db': {}, 's3': {}}
+    ingestion_bucket = "ingestion-bucket-neural-normalisers-new"
+
+    latest_timestamp = get_latest_s3_keys(ingestion_bucket,s3)
+
+
+    for table in tables:
+        data['db'][table] = fetch_data_from_table(conn, table)
+        
+        s3_key = f'{table}/{latest_timestamp}.json'
+        data['s3'][table] = fetch_from_s3(ingestion_bucket, s3_key, s3)
+
+
+    changed_tables = main_check_for_changes(data, s3)
+
+    str_timestamp = datetime.now().isoformat()
+
+    for table in changed_tables:
+        json_filename = f"{table}/{str_timestamp}.json"
+        save_to_s3(data['db'][table], ingestion_bucket, json_filename, s3)
+
+
+
+
---
+++ test/test_ingestion.py
@@ -0,0 +1,265 @@
+from src.ingestion import *
+from datetime import datetime
+from decimal import Decimal
+from unittest.mock import patch , Mock
+import pytest
+from moto import mock_aws
+import boto3
+from pprint import pprint
+import json
+from pg8000.native import Connection
+
+tables = [
+    "counterparty",
+    "currency",
+    "department",
+    "design",
+    "staff",
+    "sales_order",
+    "address",
+    "payment",
+    "purchase_order",
+    "payment_type",
+    "transaction"
+]
+
+class MockConnection:
+    def run(self, query):
+        return [
+            (1, "GBP", datetime(2022, 11, 3, 14, 20, 49), Decimal("100.0")),
+            (2, "USD", datetime(2022, 11, 3, 14, 20, 49), Decimal("200.0"))
+        ]
+    @property
+    def columns(self):
+        return [
+            {"name": "currency_id"},
+            {"name": "currency_code"},
+            {"name": "created_at"},
+            {"name": "amount"}
+        ]
+
+@pytest.fixture
+def mock_conn():
+    return MockConnection()
+
+
+class TestFetchData:
+    def test_fetch_data_from_table(self,mock_conn):
+        table_name = "currency"
+        data = fetch_data_from_table(mock_conn, table_name)
+        
+        expected_data = [
+            {
+                "currency_id": 1,
+                "currency_code": "GBP",
+                "created_at": "2022-11-03T14:20:49",
+                "amount": 100.0
+            },
+            {
+                "currency_id": 2,
+                "currency_code": "USD",
+                "created_at": "2022-11-03T14:20:49",
+                "amount": 200.0
+            }
+        ]
+        assert data == expected_data
+
+    def test_datetime_serialisation(self, mock_conn):
+        data = fetch_data_from_table(mock_conn, "currency")
+        for entry in data:
+            assert isinstance(entry["created_at"], str)
+        
+    def test_decimal_serialisation(self, mock_conn):
+        data = fetch_data_from_table(mock_conn, "currency")
+        for entry in data:
+            assert isinstance(entry["amount"],float)
+
+@pytest.fixture()
+def s3_mock():
+    with mock_aws():
+        s3 = boto3.client('s3')
+        yield s3
+
+@pytest.fixture()
+def s3_mock_with_bucket():
+    with mock_aws():
+        s3 = boto3.client('s3', region_name='eu-west-2')
+        
+        s3.create_bucket(Bucket='ingestion-bucket-neural-normalisers-new',
+                        CreateBucketConfiguration={
+                        'LocationConstraint': 'eu-west-2'}
+                        )
+        yield s3
+
+@pytest.fixture()
+def s3_mock_with_objects(s3_mock_with_bucket):
+    for i in range(10):
+        for table in tables:
+            fake_timestamp = f'{table}/2024-11-14T09:27:40.35701{i}.json'
+            s3_mock_with_bucket.put_object(Bucket='ingestion-bucket-neural-normalisers-new',
+                            Body=b'test_content',
+                            Key=fake_timestamp)
+    yield s3_mock_with_bucket
+
+expected_data = [
+            {
+                "currency_id": 1,
+                "currency_code": "GBP",
+                "created_at": "2022-11-03T14:20:49",
+                "amount": 100.0
+            },
+            {
+                "currency_id": 2,
+                "currency_code": "USD",
+                "created_at": "2022-11-03T14:20:49",
+                "amount": 200.0
+            }
+        ]
+
+class TestPutObject():
+    
+    def test_creates_object(self, s3_mock_with_bucket):
+        save_to_s3(expected_data, 'ingestion-bucket-neural-normalisers-new', 'test_object', s3_mock_with_bucket)
+
+        object_list = s3_mock_with_bucket.list_objects_v2(Bucket='ingestion-bucket-neural-normalisers-new')
+        assert object_list['Contents'][0]['Key'] == 'test_object'
+
+    def test_object_body_as_expected(self, s3_mock_with_bucket):
+        save_to_s3(expected_data, 'ingestion-bucket-neural-normalisers-new', 'test_object', s3_mock_with_bucket)
+
+        result = s3_mock_with_bucket.get_object(
+            Bucket='ingestion-bucket-neural-normalisers-new',
+            Key='test_object'
+        )['Body'].read()
+
+        assert json.loads(result) == expected_data
+
+def test_connection_to_db():
+    conn = connect_to_db()
+    assert isinstance(conn, Connection)
+    close_db_connection(conn)
+    
+class TestFetchFromS3:
+    def test_function_success(self, s3_mock_with_bucket, caplog):
+        content = {'test': 'test_value'}
+        s3_mock_with_bucket.put_object(
+        Bucket='ingestion-bucket-neural-normalisers-new',
+        Key='test.json',
+        Body=json.dumps(content)
+        )
+        with caplog.at_level(logging.INFO):
+            response = fetch_from_s3('ingestion-bucket-neural-normalisers-new', 'test.json', s3_mock_with_bucket)
+        assert response == content
+        assert "Lambda handler invoked with event" in caplog.text
+        assert "Fetching object from S3" in caplog.text
+        assert "Successfully retrieved and parsed object from S3" in caplog.text
+        
+    def test_object_not_found(self, s3_mock_with_bucket, caplog):
+        with caplog.at_level(logging.INFO):
+            response = fetch_from_s3('ingestion-bucket-neural-normalisers-new', 'non-existent.json', s3_mock_with_bucket)
+        assert "Object not found in S3" in caplog.text
+        
+    def test_invalid_json_format(self, s3_mock_with_bucket, caplog):
+        s3_mock_with_bucket.put_object(
+        Bucket='ingestion-bucket-neural-normalisers-new',
+        Key='test.json',
+        Body='Not a json'
+        )
+        with caplog.at_level(logging.INFO):
+            response = fetch_from_s3('ingestion-bucket-neural-normalisers-new', 'test.json', s3_mock_with_bucket)
+        assert "Failed to decode JSON" in caplog.text
+
+class TestFetchData:
+    def test_fetch_data_from_table(self,mock_conn):
+        table_name = "currency"
+        data = fetch_data_from_table(mock_conn, table_name)
+        
+        expected_data = [
+            {
+                "currency_id": 1,
+                "currency_code": "GBP",
+                "created_at": "2022-11-03T14:20:49",
+                "amount": 100.0
+            },
+            {
+                "currency_id": 2,
+                "currency_code": "USD",
+                "created_at": "2022-11-03T14:20:49",
+                "amount": 200.0
+            }
+        ]
+        assert data == expected_data
+
+    def test_datetime_serialisation(self, mock_conn):
+        data = fetch_data_from_table(mock_conn, "currency")
+        for entry in data:
+            assert isinstance(entry["created_at"], str)
+        
+    def test_decimal_serialisation(self, mock_conn):
+        data = fetch_data_from_table(mock_conn, "currency")
+        for entry in data:
+            assert isinstance(entry["amount"],float)
+
+class TestGetLatestS3Keys:
+    #returns a list of strings
+    #strings are valid s3 objects - mock
+    #strings returned contain the latest timestamp
+
+    def test_returns_string(self, s3_mock_with_objects):
+        result = get_latest_s3_keys('ingestion-bucket-neural-normalisers-new', s3_mock_with_objects)
+        assert isinstance(result, str)
+
+    def test_returned_strings_is_latest_timestamp(self, s3_mock_with_objects):
+        result = get_latest_s3_keys('ingestion-bucket-neural-normalisers-new', s3_mock_with_objects)
+        expected_result = '2024-11-14T09:27:40.357019'
+        assert result == expected_result
+
+class TestLambdaHandler:
+
+    expected_db_data = [
+        {"currency_id": 1, "currency_code": "GBP", "created_at": "2022-11-03T14:20:49", "amount": 100.0},
+        {"currency_id": 2, "currency_code": "USD", "created_at": "2022-11-03T14:20:49", "amount": 200.0}
+    ]
+
+    expected_s3_data = [
+        {"currency_id": 1, "currency_code": "GBP", "created_at": "2022-11-03T14:20:49", "amount": 100.0},
+        {"currency_id": 2, "currency_code": "USD", "created_at": "2022-11-03T14:20:49", "amount": 200.0},
+        {"currency_id": 3, "currency_code": "EUR", "created_at": "2022-11-03T14:20:49", "amount": 200.0}
+    ]
+
+    @patch('src.ingestion.fetch_from_s3')
+    @patch('src.ingestion.fetch_data_from_table')
+    @patch('src.ingestion.get_latest_s3_keys')
+    @patch('src.ingestion.connect_to_db')
+    @patch('src.ingestion.datetime')
+    @patch('src.ingestion.main_check_for_changes')
+    def test_lambda_handler_correct_s3_files_created(self,
+                                                mock_check_for_changes,
+                                                mock_date_time,
+                                                mock_connect_to_db,
+                                                mock_get_latest_s3_keys,
+                                                mock_fetch_data_from_table,
+                                                mock_fetch_from_s3,    
+                                                s3_mock_with_bucket):
+
+        mock_connect_to_db.return_value = MockConnection()
+        mock_get_latest_s3_keys.return_value = 'latest_timestamp'
+        mock_fetch_data_from_table.return_value = self.expected_db_data
+        mock_fetch_from_s3.return_value = self.expected_s3_data
+        mock_check_for_changes.return_value = ['currency']
+        mock_date_time.now().isoformat = Mock(return_value='latest_timestamp')   
+
+        lambda_handler(None, None)
+
+        s3_object = s3_mock_with_bucket.get_object(
+            Bucket='ingestion-bucket-neural-normalisers-new',
+            Key='currency/latest_timestamp.json'
+        )
+
+        body = s3_object['Body'].read().decode()
+        body = json.loads(body)
+
+        assert body == self.expected_db_data
+
+
+
