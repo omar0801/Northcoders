e085c9b7b511438be53faf56d424d391afc3bf7e
JIEguare <jayeguare@yahoo.co.uk>
2024-11-20 12:33:32 +0000

    test suite done

--- src/fact_sales_order.py
+++ src/fact_sales_order.py
@@ -4,6 +4,7 @@ import pandas as pd
 def create_fact_sales_order_table(filepath):
     sales_order_df = pd.read_json (filepath, orient='records', encoding='utf-8')
     sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_at']).dt.date
+    sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_date'], format='%y%m%d')
     sales_order_df['created_time'] = pd.to_datetime(sales_order_df['created_at']).dt.time
     sales_list = sales_order_df['sales_order_id'].tolist()
     sales_order_df['sales_record_id'] = [i for i in range(1, len(sales_list) + 1)]
@@ -11,9 +12,14 @@ def create_fact_sales_order_table(filepath):
     new_date_list = [pd.to_datetime(date.replace('T', ' ')) for date in date_list]
     sales_order_df['last_update'] = new_date_list
     sales_order_df['last_updated_date'] = pd.to_datetime(sales_order_df['last_update']).dt.date
+    sales_order_df['last_updated_date'] = pd.to_datetime(sales_order_df['last_updated_date'], format='%y%m%d')
     sales_order_df['last_updated_time'] = pd.to_datetime(sales_order_df['last_update']).dt.time
+    sales_order_df['agreed_payment_date'] = pd.to_datetime(sales_order_df['agreed_payment_date']).dt.date
+    sales_order_df['agreed_payment_date'] = pd.to_datetime(sales_order_df['agreed_payment_date'], format='%y%m%d')
+    sales_order_df['agreed_delivery_date'] = pd.to_datetime(sales_order_df['agreed_delivery_date']).dt.date
+    sales_order_df['agreed_delivery_date'] = pd.to_datetime(sales_order_df['agreed_delivery_date'], format='%y%m%d')
     sales_order_df['sales_staff_id'] = sales_order_df['staff_id']
-    fact_sales_order_df = sales_order_df[['sales_record_id',
+    fact_sales_order = sales_order_df[['sales_record_id',
                                           'sales_order_id', 'created_date',
                                           'created_time', 'last_updated_date',
                                           'last_updated_time', 'sales_staff_id',
@@ -21,9 +27,5 @@ def create_fact_sales_order_table(filepath):
                                           'currency_id', 'design_id', 'agreed_payment_date',
                                           'agreed_delivery_date',
                                           'agreed_delivery_location_id']]
-    fact_sales_order = fact_sales_order_df.set_index('sales_record_id')
     return fact_sales_order
-
-result = create_fact_sales_order_table('data/sales_order.json')
-print(result) 
     
