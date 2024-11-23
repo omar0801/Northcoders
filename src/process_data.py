65483c914b89439a2582bc0356a71f3ab66448f9
JIEguare <jayeguare@yahoo.co.uk>
2024-11-23 21:32:01 +0000

    versioning of facts table

--- src/process_data.py
+++ src/process_data.py
@@ -16,7 +16,7 @@ tables = [
     "currency",
     # "department",
     # "staff",
-    # "sales_order",
+    "sales_order",
     # "payment",
     # "purchase_order",
     # "payment_type",
@@ -66,7 +66,33 @@ def create_dim_currency(currency_data):
     print(dim_currency)
     return {'dataframe': dim_currency, 'table_name': 'dim_currency'}
 
-
+def create_fact_sales_order_table(sales_data):
+    sales_order_df = pd.DataFrame(sales_data)
+    sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_at']).dt.date
+    sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_date'], format='%y%m%d')
+    sales_order_df['created_time'] = pd.to_datetime(sales_order_df['created_at']).dt.time
+    sales_order_df['sales_record_id'] = range(1, len(sales_order_df) + 1)
+    date_list = sales_order_df['last_updated'].tolist()
+    new_date_list = [pd.to_datetime(date.replace('T', ' ')) for date in date_list]
+    sales_order_df['last_update'] = new_date_list
+    sales_order_df['last_updated_date'] = pd.to_datetime(sales_order_df['last_update']).dt.date
+    sales_order_df['last_updated_date'] = pd.to_datetime(sales_order_df['last_updated_date'], format='%y%m%d')
+    sales_order_df['last_updated_time'] = pd.to_datetime(sales_order_df['last_update']).dt.time
+    sales_order_df['agreed_payment_date'] = pd.to_datetime(sales_order_df['agreed_payment_date']).dt.date
+    sales_order_df['agreed_payment_date'] = pd.to_datetime(sales_order_df['agreed_payment_date'], format='%y%m%d')
+    sales_order_df['agreed_delivery_date'] = pd.to_datetime(sales_order_df['agreed_delivery_date']).dt.date
+    sales_order_df['agreed_delivery_date'] = pd.to_datetime(sales_order_df['agreed_delivery_date'], format='%y%m%d')
+    sales_order_df['sales_staff_id'] = sales_order_df['staff_id']
+    fact_sales_order = sales_order_df[['sales_record_id',
+                                          'sales_order_id', 'created_date',
+                                          'created_time', 'last_updated_date',
+                                          'last_updated_time', 'sales_staff_id',
+                                          'counterparty_id', 'units_sold', 'unit_price',
+                                          'currency_id', 'design_id', 'agreed_payment_date',
+                                          'agreed_delivery_date',
+                                          'agreed_delivery_location_id']]
+    fact_sales_order = fact_sales_order.assign(status='current') 
+    return {'dataframe': fact_sales_order, 'table_name': 'facts_sales'}
 
 def write_dataframe_to_s3(df_dict, s3):
     str_timestamp = datetime.now().isoformat()
@@ -95,7 +121,7 @@ def check_for_fact_sales_parquet(s3_client):
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     ingestion_bucket = "ingestion-bucket-neural-normalisers-new"
-
+    processed_bucket = "processed-bucket-neural-normalisers"
     data = {}
 
     for table in tables:        
@@ -112,17 +138,35 @@ def lambda_handler(event, context):
    
 
     if not check_for_fact_sales_parquet(s3):
-        #create facts_sales parquet
-        pass
+        sales_df = create_fact_sales_order_table(data['sales_order'])
+        write_dataframe_to_s3(sales_df, s3)
     else:
         change_log_timestamp = get_latest_s3_keys(ingestion_bucket,s3, 'changes_log/sales_order')
         sales_change_log_key = f'changes_log/sales_order/{change_log_timestamp}.json'
         change_log_data = fetch_from_s3(ingestion_bucket, sales_change_log_key, s3)
         if any(change_log_data.values()):
-            #call update facts_table function
-            pass
-
-
+            added_records = change_log_data['sales_order']['additions']
+            if added_records:
+                added_df = create_fact_sales_order_table(added_records)['dataframe']
+            deleted_records = change_log_data['sales_order']['deletions']
+            if deleted_records:
+                sales_order_timestamp = get_latest_s3_keys(processed_bucket, s3, 'processed_data/facts_sales')
+                obj = s3.get_object(Bucket=processed_bucket,
+                Key= f'processed_data/facts_sales/{sales_order_timestamp}.parquet')
+            
+                df = pd.read_parquet(BytesIO(obj['Body'].read()))
+                for record in deleted_records:
+                    df.loc[df['sales_order_id']== record, 'status'] = 'deleted'
+            
+            sales_order_timestamp = get_latest_s3_keys(processed_bucket, s3, 'processed_data/facts_sales')
+            obj = s3.get_object(Bucket=processed_bucket,
+            Key= f'processed_data/facts_sales/{sales_order_timestamp}.parquet')
+            
+            df = pd.read_parquet(BytesIO(obj['Body'].read()))
+            frames = [df, added_df]
+            output = pd.concat(frames)
+            
+        
     for dataframe in dataframes:
         write_dataframe_to_s3(dataframe, s3)
 
