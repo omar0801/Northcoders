54cd2ccf88ba381e2e4d142dfac61e6f678e5d37
Nana <nanagalaxy385@gmail.com>
2024-11-27 10:49:52 +0000

    docstrings added for phase 2 lambda function

--- src/process_data.py
+++ src/process_data.py
@@ -21,6 +21,19 @@ tables = [
 ]
 
 def get_latest_s3_keys(bucket,s3_client, table_name):
+
+    """
+    Finds the latest timestamped file for a specific table in an S3 bucket.
+
+    Args:
+        bucket: The S3 bucket name.
+        s3_client: The S3 client to interact with S3.
+        table_name: The name of the table to look for.
+
+    Returns:
+        str: The latest timestamp of the file.
+    """
+    
     all_objects = s3_client.list_objects_v2(Bucket=bucket, Prefix=table_name)
     table_name += '/'
     all_key_timestamps = [item['Key'][-31:-5] for item in all_objects['Contents']]
@@ -28,6 +41,19 @@ def get_latest_s3_keys(bucket,s3_client, table_name):
     return latest_timestamp
 
 def fetch_from_s3(bucket, key,s3 ):
+
+    """
+    Fetches and reads JSON data from S3.
+
+    Args:
+        bucket: Name of the S3 bucket.
+        key: Key of the object to retrieve.
+        s3: S3 client instance.
+
+    Returns:
+        dict: Parsed JSON content from the S3 object.
+    """
+
     logger.info('Lambda handler invoked with event: %s', json.dumps([bucket, key]))
     try:
         logger.info("Fetching object from S3: bucket=%s, key=%s", bucket, key)
@@ -43,6 +69,17 @@ def fetch_from_s3(bucket, key,s3 ):
 
 
 def create_dim_location(address_data):
+
+    """
+    Creates the table 'dim_location' from address data.
+
+    Args:
+        address_data (list): List of address records.
+
+    Returns:
+        dict: Dataframe and table name for the dim_location table.
+    """
+
     address_df = pd.DataFrame(address_data)
     
     location_df = address_df.drop(['created_at', 'last_updated'], axis=1)
@@ -51,18 +88,51 @@ def create_dim_location(address_data):
     return {'dataframe': location_df, 'table_name': 'dim_location'}
 
 def create_dim_design(design_JSON):
+
+    """
+    Creates the table 'dim_design' from design data.
+
+    Args:
+        design_JSON (list): List of design records.
+
+    Returns:
+        dict: Dataframe and table name for the dim_design table.
+    """
+
     design_df = pd.DataFrame(design_JSON)
     dim_design = design_df.drop(['created_at', 'last_updated'], axis=1)
 
     return {'dataframe': dim_design, 'table_name': 'dim_design'}
 
 def create_dim_currency(currency_data):
+
+    """
+    Creates the table 'dim_currency' from currency data.
+
+    Args:
+        currency_data (list): List of currency records.
+
+    Returns:
+        dict: Dataframe and table name for the dim_currency table.
+    """
+
     df = pd.DataFrame(currency_data)
     dim_currency = df[['currency_id', 'currency_code']]
     dim_currency['currency_name'] = ["Great British Pounds", "US Dollars", 'Euros']
     return {'dataframe': dim_currency, 'table_name': 'dim_currency'}
 
 def create_fact_sales_order_table(sales_data):
+
+    """
+    Creates the fact sales order table from sales data.
+
+    Args:
+        sales_data (list): List of sales order records.
+
+    Returns:
+        dict: Dataframe and table name for the fact sales order table.
+    """
+
     sales_order_df = pd.DataFrame(sales_data)
     sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_at'], format='ISO8601').dt.date
     sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_date'], format='%y%m%d')
@@ -90,6 +160,18 @@ def create_fact_sales_order_table(sales_data):
     return {'dataframe': fact_sales_order, 'table_name': 'fact_sales_order'}
 
 def create_dim_counterparty(counterparty, address):
+
+    """
+    Creates the table 'dim_counterparty' by merging 'counterparty' and 'address' data.
+
+    Args:
+        counterparty (list): List of counterparty records.
+        address (list): List of address records.
+
+    Returns:
+        dict: Dataframe and table name for the dim_counterparty table.
+    """
+
     address = pd.DataFrame(address)
     counterparty = pd.DataFrame(counterparty)
     address_counterparty = pd.merge(counterparty, address, how='left', left_on='legal_address_id', right_on='address_id')
@@ -101,6 +183,18 @@ def create_dim_counterparty(counterparty, address):
     return {'dataframe': dim_counterparty, 'table_name': 'dim_counterparty'}
 
 def create_dim_staff(staff_data, department_data):
+
+    """
+    Creates the table 'dim_staff' by merging staff and department data.
+
+    Args:
+        staff_data (list): List of staff records.
+        department_data (list): List of department records.
+
+    Returns:
+        dict: Dataframe and table name for the dimensional staff table.
+    """
+
     dataframe_staff = pd.DataFrame(staff_data)
     dataframe_dept = pd.DataFrame(department_data)
 
@@ -114,6 +208,17 @@ def create_dim_staff(staff_data, department_data):
     return {'dataframe': dim_staff, 'table_name': 'dim_staff'}
 
 def create_dim_date(start_date = '2022-01-01', end_date='2025-12-31'):
+
+    """
+    Creates a table 'dim_date' for a given range.
+
+    Args:
+        start_date (str): Start date in 'YYYY-MM-DD' format.
+        end_date (str): End date in 'YYYY-MM-DD' format.
+
+    Returns:
+        dict: Dataframe and table name for the dim_date table.
+    """
 
     try:
         df_start_date = pd.Timestamp(start_date)  
@@ -142,6 +247,15 @@ def create_dim_date(start_date = '2022-01-01', end_date='2025-12-31'):
         return "Incorrect Date"
 
 def write_dataframe_to_s3(df_dict, s3):
+
+    """
+    Writes a dataframe to S3, stored as a parquet file.
+
+    Args:
+        df_dict (dict): Dictionary containing the dataframe and table name.
+        s3 (boto3.client): S3 client instance.
+    """
+
     str_timestamp = datetime.now().isoformat()
     bucket_name = "processed-bucket-neural-normalisers"
     key = f"processed_data/{df_dict['table_name']}/{str_timestamp}.parquet"
@@ -158,6 +272,17 @@ def write_dataframe_to_s3(df_dict, s3):
     )
 
 def check_for_fact_sales_parquet(s3_client):
+
+    """
+    Checks if a fact sales parquet file exists in the S3 bucket.
+
+    Args:
+        s3_client (boto3.client): S3 client instance.
+
+    Returns:
+        int: Number of matching parquet files.
+    """
+
     fact_sales_parquet = s3_client.list_objects_v2(
         Bucket="processed-bucket-neural-normalisers",
         Prefix="processed_data/facts_sales/2"
@@ -166,6 +291,18 @@ def check_for_fact_sales_parquet(s3_client):
 
 
 def lambda_handler(event, context):
+
+    """
+    Starting point for AWS Lambda function execution. Processes data from S3 to create d'im' and 'fact' tables.
+
+    Args:
+        event (dict): Event triggering the Lambda.
+        context (object): Context of the Lambda execution.
+
+    Returns:
+        None
+    """
+
     s3 = boto3.client('s3')
     ingestion_bucket = "ingestion-bucket-neural-normalisers-new"
     processed_bucket = "processed-bucket-neural-normalisers"
